e73ffc0a38963ed30275a911e319c07891a1a8e8
Matt Nwerem <MattNwerem@Matts-MacBook-Pro-5.local>
2019-10-01 11:14:02 -0700

    comments added to RandGrid

--- RandGrid.h
+++ RandGrid.h
@@ -1,3 +1,16 @@
+/*Michael Kulinich
+2328386
+kulinich@chapman.edu
+Matthew Nwerem
+2277158
+nwere100@mail.chapman.edu
+
+
+RandGrid.h
+This is header file for the RandGrid class, a class that is used when the user
+asks for a random grid to be created rather than an imported one.
+*/
+
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
@@ -7,22 +20,16 @@ class RandGrid
 {
   public:
     RandGrid(int rowDim, int colDim, double randProb);
-    ~RandGrid();
+    //Deconstructor of RandGrid is not needed; the grid is deleted in Simulation
 
     char** getCurrentGrid();
     char** getNextGrid();
 
-
     int rowDimension;
     int columnDimension;
     double randProbability;
+
   private:
     char **myCurrentGrid;
     char **myNextGrid;
-
-  //  char updateGrid();
-  //  char printGrid();
-
-
-
 };
